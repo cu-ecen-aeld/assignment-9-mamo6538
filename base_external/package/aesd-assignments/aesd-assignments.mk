7b9c9d0c5b40cf331fda0708b3769d53f4006d53
Madeleine Monfort <mamo6538@colorado.edu>
2024-02-10 18:16:04 -0800

    updated package to most correct git version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5baf7c2c8b03bba02477ab9729b61b510292c592
+AESD_ASSIGNMENTS_VERSION = cb6cc3f7e8e30cd35c5323bed788bddc674e1fda
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
