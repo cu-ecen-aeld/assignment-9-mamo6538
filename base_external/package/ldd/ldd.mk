722de734796231d682f0d618280c51673a873260
Madeleine Monfort <mamo6538@colorado.edu>
2024-03-07 19:45:13 -0800

    initial ldd package creation

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,13 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+LDD_VERSION = a58d639419333b8dfa962bae9d9eda481be83634
+LDD_SITE = git@github.com:cu-ecen-aeld/assignments-7-mamo6538.git
+LDD_SITE_METHOD = git
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
