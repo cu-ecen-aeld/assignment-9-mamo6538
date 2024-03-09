a0791b90fdf724af290552d68b69389e2178ba92
Madeleine Monfort <mamo6538@colorado.edu>
2024-03-09 14:56:26 -0800

    fixing buildroot version

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,8 +6,11 @@
 ##############################################################
 
 LDD_VERSION = a58d639419333b8dfa962bae9d9eda481be83634
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-7-mamo6538.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-mamo6538.git
 LDD_SITE_METHOD = git
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
